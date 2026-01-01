2d7adb76f1a81373ffc2118c4bfcb27d0b7621dd
Adrish <runakarak@gmail.com>
2026-01-01 23:13:19 +0530

    gh

--- Taskmanager/src/pages/Auth/Login.jsx
+++ Taskmanager/src/pages/Auth/Login.jsx
@@ -3,6 +3,8 @@ import { Link, useNavigate } from "react-router-dom"
 import AuthLayout from '../../Components/layouts/AuthLayout';
 import Input from '../../Components/Inputs/Input';
 import { validateEmail } from '../../utils/helper';
+import axiosInstance from '../../utils/axiosinstance';
+import { API_PATHS } from '../../utils/apiPaths';
 
 const Login = () => {
     const [email, setEmail] = useState("");
@@ -28,9 +30,29 @@ const Login = () => {
 
         //Login API call
         try {
+            const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+                email,
+                password
+            });
 
+            const { token, role } = response.data;
+
+            if (token) {
+                localStorage.setItem("token", token);
+
+                //Redirect based on role
+                if (role === "admin") {
+                    navigate("/admin/dashboard");
+                } else {
+                    navigate("/user/dashboard");
+                }
+            }
         } catch (error) {
-
+            if (error.response && error.response.data.message) {
+                setError(error.response.data.message);
+            } else {
+                setError("Something went wrong");
+            }
         }
     }
     return <AuthLayout>
