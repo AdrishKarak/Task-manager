b3d26777eb7590ff166d0d82e8c5d65421d2aac0
Adrish <runakarak@gmail.com>
2026-01-06 19:45:06 +0530

    zx

--- Taskmanager/src/App.jsx
+++ Taskmanager/src/App.jsx
@@ -10,7 +10,7 @@ import CreateTask from "./pages/Admin/CreateTask.jsx";
 import ManageUsers from "./pages/Admin/ManageUsers.jsx";
 import MyTasks from "./pages/User/MyTasks.jsx";
 import ViewTasksdetails from "./pages/User/ViewTasksdetails.jsx";
-import UserProvider, { UserContext } from '../context/userContext.jsx';
+import UserProvider, { UserContext } from './context/userContext.jsx';
 import { useContext } from 'react';
 
 const App = () => {
