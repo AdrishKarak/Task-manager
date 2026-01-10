e8114336bc43bb1b96e4708bd0e1cc8b061377ec
Adrish <runakarak@gmail.com>
2026-01-11 00:48:00 +0530

    ghj

--- Taskmanager/src/Components/layouts/SideMenu.jsx
+++ Taskmanager/src/Components/layouts/SideMenu.jsx
@@ -1,6 +1,33 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../context/userContext';
+import { useNavigate } from 'react-router-dom';
+import { SIDE_MENU_DATA, SIDE_MENU_USER_DATA } from '../../utils/data';
 
 const SideMenu = ({ activeMenu }) => {
+    const { user, clearUser } = useContext(UserContext);
+    const [sideMenuData, setSideMenuData] = useState([]);
+
+    const navigate = useNavigate();
+
+    const handleClick = (route) => {
+        if (route === "logout") {
+            handleLogout();
+            return;
+        }
+        navigate(route);
+    };
+
+    const handleLogout = () => {
+        localStorage.clear();
+        clearUser();
+        navigate("/login");
+    };
+
+    useEffect(() => {
+        if (user) {
+            setSideMenuData(user?.role === 'admin' ? SIDE_MENU_DATA : SIDE_MENU_USER_DATA)
+        } return () => { }
+    }, [user]);
 
     return (
         <div>
