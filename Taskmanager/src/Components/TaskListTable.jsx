46ded4da2d03161b35227821eeb5f255847d6547
Adrish <runakarak@gmail.com>
2026-01-18 01:58:28 +0530

    fgh

---
+++ Taskmanager/src/Components/TaskListTable.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import moment from 'moment';
+
+const TaskListTable = ({ tableData }) => {
+    const getStatusBadgeColor = (status) => {
+        switch (status) {
+            case 'Pending':
+                return 'bg-purple-200 text-purple-500 border border-purple-200';
+            case 'In Progress':
+                return 'bg-yellow-200 text-yellow-500 border border-yellow-200';
+            case 'Completed':
+                return 'bg-green-200 text-green-500 border border-green-200';
+            default:
+                return 'bg-gray-200 text-gray-500 border border-gray-200';
+        }
+    };
+
+    const getpriorityBadgeColor = (priority) => {
+        switch (priority) {
+            case 'Low':
+                return 'bg-green-200 text-green-500 border border-green-200';
+            case 'Medium':
+                return 'bg-yellow-200 text-yellow-500 border border-yellow-200';
+            case 'High':
+                return 'bg-red-200 text-red-500 border border-red-200';
+            default:
+                return 'bg-gray-200 text-gray-500 border border-gray-200';
+        }
+    }
+
+    return (
+        <div className='overflow-x-auto p-0 rounded-lg mt-3'>
+            <table className='min-w-full'>
+                <thead>
+                    <tr className='text-left'>
+                        <th className='py-3 px-4 text-gray-800 font-medium text-[13px]'>Name</th>
+                        <th className='py-3 px-4 text-gray-800 font-medium text-[13px]'>Status</th>
+                        <th className='py-3 px-4 text-gray-800 font-medium text-[13px]'>Priority</th>
+                        <th className='py-3 px-4 text-gray-800 font-medium text-[13px] hidden md:table-cell'>Created On</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableData.map((task) => (
+                        <tr key={task.id} className='border-t border-gray-200'>
+                            <td className='my-3 mx-4 text-gray-700 text-[13px] line-clamp-1 overflow-hidden'>{task.title}</td>
+                            <td className='px-4 py-4'><span className={`px-2 py-1 text-xs rounded inline-block ${getStatusBadgeColor(task.status)}`}>{task.status}</span></td>
+                            <td className='px-4 py-4'><span className={`px-2 py-1 text-xs rounded inline-block ${getpriorityBadgeColor(task.priority)}`}>{task.priority}</span></td>
+                            <td className='px-4 py-4 hidden md:table-cell text-nowrap text-gray-700 text-[13px]'>{task.createdAt ? moment(task.createdAt).format('DD-MM-YYYY') : 'N/A'}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+export default TaskListTable;   
--- Taskmanager/src/pages/Admin/DashboardAdmin.jsx
+++ Taskmanager/src/pages/Admin/DashboardAdmin.jsx
@@ -6,6 +6,10 @@ import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../../utils/axiosinstance';
 import { API_PATHS } from '../../utils/apiPaths';
 import moment from 'moment';
+import { addThousandSeparator } from '../../utils/helper';
+import InfoCard from '../../Components/Cards/InfoCard';
+import { LuArrowRight } from 'react-icons/lu';
+import TaskListTable from '../../Components/TaskListTable';
 
 const DashboardAdmin = () => {
     useUserAuth();
@@ -28,6 +32,10 @@ const DashboardAdmin = () => {
         }
     }
 
+    const onSeeMore = () => {
+        navigate('/admin/tasks');
+    }
+
     useEffect(() => {
         getDashboardData();
 
@@ -44,7 +52,47 @@ const DashboardAdmin = () => {
                         </p>
                     </div>
                 </div>
+                <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3 md:gap-6 mt-5'>
+                    <InfoCard
+                        label="Total tasks"
+                        value={addThousandSeparator(dashboardData?.charts?.taskDistribution?.All || 0)}
+                        color="bg-blue-500"
+                    />
+
+                    <InfoCard
+                        label="Pending tasks"
+                        value={addThousandSeparator(dashboardData?.charts?.taskDistribution?.Pending || 0)}
+                        color="bg-violet-500"
+                    />
+
+                    <InfoCard
+                        label="In Progress tasks"
+                        value={addThousandSeparator(dashboardData?.charts?.taskDistribution?.InProgress || 0)}
+                        color="bg-yellow-600"
+                    />
+
+                    <InfoCard
+                        label="Completed tasks"
+                        value={addThousandSeparator(dashboardData?.charts?.taskDistribution?.Completed || 0)}
+                        color="bg-green-500"
+                    />
+                </div>
             </div>
+
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-6 my-4 md:my-6'>
+                <div className='md:col-span-2'>
+                    <div className='card'>
+                        <div className='flex items-center justify-between'>
+                            <h5 className='text-lg'>Recent Tasks</h5>
+                            <button className='card-btn' onClick={onSeeMore}>See All
+                                <LuArrowRight className='text-base' />
+                            </button>
+                        </div>
+                        <TaskListTable tableData={dashboardData?.recentTasks || []} />
+                    </div>
+                </div>
+            </div>
+
         </DashboardLayout>
     );
 };
